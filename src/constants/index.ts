89be847b210ac6f62a8fc85d6b0ece5c731c4267
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-14 21:31:14 +0300

    feat: add constants for view data

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const HEADER_KEYS: Array<string> = [
+  "groupName",
+  "course",
+  "studentsNumber",
+  "semestr",
+];
+
+export const BODY_KEYS: Array<string> = [
+  "lecturesHours",
+  "laboratoryHours",
+  "practicHours",
+  "seminarHours",
+  "offset",
+  "exam",
+  "countStudents",
+  "additionalInfo",
+];
+
+export const PODGROUPS_KEYS: Array<string> = [
+  "laboratoryTeacher",
+  "lectureTeacher",
+  "practiceTeacher",
+  "seminarTeacher",
+  "examTeacher",
+  "offsetTeacher",
+];
