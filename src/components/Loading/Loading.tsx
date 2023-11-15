895c967ac1fa78d51e184651b2e10f44b8f2bf49
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-15 21:55:19 +0300

    feat: add loader

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import "./Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <span className="loading-first"></span>
+      <span className="loading-second"></span>
+      <span className="loading-third"></span>
+    </div>
+  );
+};
+
+export default Loading;
