ab0dc62cca3a1e3f79f03c6ccf1636603d2ac777
davidpinkevich <davidpinkevich95@gmail.com>
2023-11-15 17:13:48 +0300

    feat: add styles for select

---
+++ src/components/Item/ItemSelect/styles.ts
@@ -0,0 +1,41 @@
+type TStyles = {
+  input: {
+    fontSize: number;
+    width: string;
+    height: string;
+    textAlign: "center" | "left" | "right";
+    fontFamily: string;
+    color: string;
+  };
+  option: {
+    fontSize: number;
+    justifyContent: "center";
+    height: string;
+    color: string;
+    textAlign: "center" | "left" | "right";
+  };
+  item: {
+    whiteSpace: string;
+  };
+};
+
+export const styles: TStyles = {
+  input: {
+    fontSize: 16,
+    width: "100%",
+    height: "40px",
+    textAlign: "center",
+    fontFamily: "Roboto",
+    color: "rgb(77, 88, 103)",
+  },
+  option: {
+    fontSize: 16,
+    textAlign: "center",
+    height: "50px",
+    color: "rgb(106, 119, 136)",
+    justifyContent: "center",
+  },
+  item: {
+    whiteSpace: "initial",
+  },
+};
